963e4024e90b33db19f5f366cedda8b43b033739
MMCraftin <MMCraftin@users.noreply.github.com>
2018-08-01 09:13:27 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,62 @@
+/*
+ * JavaScript (using jQuery) POST request to send a message to a Discord channel via a webhook.
+ * See more options/info about it @ https://discordapp.com/developers/docs/resources/webhook#execute-webhook
+ */
 
-
-$("#start").click(function() {
-    $(".main").addClass("animated bounceOutUp")
-    setTimeout(function() {
-        $(".main").hide()
-
-        $("#numer").html("0")
-        $("#denom").html($("#msgamt").val())
-
-        $(".progress-div").addClass("animated bounceInUp")
-        $(".progress-div").show()
-
-        for(var i = 0; i < $("#msgamt").val(); i++) {
-            setTimeout(function() {
-                $.ajax({
-                    type: "POST",
-                    url: $("#webhook-url").val(),
-                    data: JSON.stringify({
-                        content: ($("#msg").val() == "" ? "Hide yer webhooks!" : $("#msg").val()),
-                        username: ($("#username").val() == "" ? null : $("#username").val()),
-                        avatar_url: ($("#avatar").val() == "" ? null : $("#avatar").val()),
-                    })
-                })
-                $("#numer").html(parseInt($("#numer").html()) + 1)
-            }, i * 500)
-        }
-    }, 2000)
-})
+$.ajax({
+  type: 'POST',
+  // The webhook URL.
+  url: '_',
+  // Message data.
+  data: JSON.stringify({
+    // The username to be displayed.
+    username: 'Nobody',
+    // The avatar to be displayed.
+    avatar_url: 'https://cdn.discordapp.com/avatars/411256446638882837/9a12fc7810795ded801fdb0401db0be6.png',
+    // Contents of the message to be sent.
+    content: 'Message content',
+    // Embeds to be sent.
+    embeds: [{
+      // Embed title - link on 2nd row.
+      title: 'Title',
+      // Embed description - text on 3rd row.
+      description: 'Description',
+      // Link for title and thumbnail.
+      url: 'https://gist.github.com/TheDragonRing/ea61c8d21db17913a43da92efe0de634',
+      // Decimal number colour of the side of the embed.
+      color: 11730954,
+      // Embed image - picture below description (and fields).
+      image: {
+        url: 'https://cdn.discordapp.com/avatars/411256446638882837/9a12fc7810795ded801fdb0401db0be6.png'
+      },
+      // Embed author - icon next to text at top (text is a link).
+      author: {
+        name: 'TheDragonRing',
+        url: 'https://thedragonring.me',
+        icon_url: 'https://avatars0.githubusercontent.com/u/16874139'
+      },
+      // Embed thumbnail - small image in top right corner.
+      thumbnail: {
+        url: 'https://cdn.discordapp.com/avatars/411256446638882837/9a12fc7810795ded801fdb0401db0be6.png'
+      },
+      // Custom embed fields with a bold title/name, and normal content/value below title - located below description, above image.
+      fields: [{
+        name: 'Field',
+        value: 'Field value'
+      }],
+      // Embed footer - icon next to text at bottom.
+      footer: {
+        text: 'Footer',
+        icon_url: 'https://cdn.discordapp.com/avatars/411256446638882837/9a12fc7810795ded801fdb0401db0be6.png'
+      }
+    }]
+  }),
+  // Content type.
+  contentType: 'application/json',
+  // Success callback.
+  success: function (data) { },
+  // Error callback.
+  error: function (data) {
+    alert(data.responseText);
+  }
+});
